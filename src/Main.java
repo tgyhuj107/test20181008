45b34f9eec2fd44357f259807ad5b89d3b1ecca3
tgyhuj107 <40759247+tgyhuj107@users.noreply.github.com>
2018-10-08 20:39:36 +0800

    fin in 10/08

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         //01大小寫轉換
-//        Scanner scn =new Scanner(System.in);
-//        int ch =scn.next().charAt(0);
-//        //A -->65 Z-->90 a-->97 z-->122
-//        if (ch>=65 && ch<=90){//大寫
-//            System.out.println((char)(ch+32));
-//        }
-//        if (ch>=97 && ch<=122){//大寫
-//            System.out.println((char)(ch-32));
-//        }
+        Scanner scn =new Scanner(System.in);
+        int ch =scn.next().charAt(0);
+        //A -->65 Z-->90 a-->97 z-->122
+        if (ch>=65 && ch<=90){//大寫
+            System.out.println((char)(ch+32));
+        }
+        if (ch>=97 && ch<=122){//大寫
+            System.out.println((char)(ch-32));
+        }
         //02奇偶數
 //        Scanner scn =new Scanner(System.in);
 //        int n=scn.nextInt();
@@ -23,27 +23,27 @@ public class Main {
 //            System.out.println("Odd");
 //        }
         //03
-        Scanner scn =new Scanner(System.in);
-        int dollar =scn.nextInt();
-        int th =dollar/1000;
-        dollar=dollar%1000;
-        int h =dollar/100;
-        dollar%=100;
-        int ten = dollar/10;
-        int d =dollar%10;
-        if (th>0){
-            System.out.print(th+"thousand");
-        }
-        if (h>0){
-            System.out.print(h+"hundred");
-        }
-        if (ten>0){
-            System.out.print(ten+"ten");
-        }
-        if (d>0){
-            System.out.print(d+"dollar");
-        }
-        System.out.println();
+//        Scanner scn =new Scanner(System.in);
+//        int dollar =scn.nextInt();
+//        int th =dollar/1000;
+//        dollar=dollar%1000;
+//        int h =dollar/100;
+//        dollar%=100;
+//        int ten = dollar/10;
+//        int d =dollar%10;
+//        if (th>0){
+//            System.out.print(th+"thousand");
+//        }
+//        if (h>0){
+//            System.out.print(h+"hundred");
+//        }
+//        if (ten>0){
+//            System.out.print(ten+"ten");
+//        }
+//        if (d>0){
+//            System.out.print(d+"dollar");
+//        }
+//        System.out.println();
         //04
 //        Scanner scn =new Scanner(System.in);
 //        int x=scn.nextInt();
